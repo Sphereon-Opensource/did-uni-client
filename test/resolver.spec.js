1d258610c86c9f7a3fbd84365ebeb4187c348464
Bram ten Cate <btencate@sphereon.com>
2021-08-06 14:34:39 +0200

    ONTO-3 indentation

--- test/resolver.spec.js
+++ test/resolver.spec.js
@@ -7,8 +7,8 @@ describe('resolve identifier', () => {
   const identifier = 'did:btcr:xz35-jznz-q6mr-7q6';
 
   nock(config.resolverUrl)
-  .get(`/1.0/identifiers/${identifier}`)
-  .reply(200, {id: identifier});
+    .get(`/1.0/identifiers/${identifier}`)
+    .reply(200, {id: identifier});
 
   it('should match did document id with identifier', async () => {
     const didDDocument = await resolve(identifier);

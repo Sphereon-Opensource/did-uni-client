7f90a9d0fab8e02d3b65273bd245b3aaf99bd694
Bram ten Cate <btencate@sphereon.com>
2021-08-06 14:33:18 +0200

    ONTO-3 prettier

--- src/resolver/resolver.ts
+++ src/resolver/resolver.ts
@@ -8,5 +8,5 @@ const resolverUrl = process.env.RESOLVER_URL || config.resolverUrl;
 
 export function resolve(identifier: string) {
   const url = `${resolverUrl}/1.0/identifiers/${identifier}`;
-  return axios.default.get(url).then(result => result.data);
+  return axios.default.get(url).then((result) => result.data);
 }

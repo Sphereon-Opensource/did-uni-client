c26576a3fd73f39b555858dcb80d367c0274f913
Bram ten Cate <btencate@sphereon.com>
2021-08-10 10:06:38 +0200

    ONTO-3 test fixes

--- test/registrar.spec.js
+++ test/registrar.spec.js
@@ -1,7 +1,8 @@
+const {Registrar} = require('../src/registrar/Registrar');
+
 const nock = require('nock');
 
 const config = require('../src/config');
-const {create, update, deactivate} = require('../src/registrar/registrar');
 
 const didDocument = {
   '@context': 'https://w3id.org/did/v1',
@@ -27,6 +28,35 @@ const didDocument = {
   }],
 };
 
+describe('setting a url', () => {
+  it('should use config / environment url when no url is provided', async () => {
+    const resolver = new Registrar();
+    expect(resolver.createUrl).toEqual(config.registrarUrlCreate);
+  });
+
+  it('should use given create url when provided by setter', async () => {
+    const otherResolver = 'https://other.registrar.io/1.0/create';
+    const registrar = new Registrar();
+    registrar.setCreateURL(otherResolver);
+    expect(registrar.createUrl).toEqual(otherResolver);
+  });
+
+  it('should use given create url when provided by setter', async () => {
+    const otherResolver = 'https://other.registrar.io/1.0/update';
+    const registrar = new Registrar();
+    registrar.setUpdateURL(otherResolver);
+    expect(registrar.updateUrl).toEqual(otherResolver);
+  });
+
+  it('should use given deactivate url when provided by setter', async () => {
+    const otherResolver = 'https://other.registrar.io/1.0/deactivate';
+    const registrar = new Registrar();
+    registrar.setDeactivateURL(otherResolver);
+    expect(registrar.deactivateUrl).toEqual(otherResolver);
+  });
+});
+
+
 describe('create identity', () => {
   const method = 'btcr';
   const options = {
@@ -36,12 +66,14 @@ describe('create identity', () => {
     didDocument
   };
 
-  nock(`${config.registrarUrl}/1.0/create`)
+  nock(config.registrarUrlCreate)
     .post(`?method=${method}`, options)
     .reply(200, {jobId: '6d85bcd0-2ea3-4288-ab00-15afadd8a156'});
 
-  it('should return identity creation job', async () => {
-    const job = await create(method, options);
+  it('should return identity creation job when using constructor', async () => {
+    const registrar = new Registrar(config.registrarUrlCreate);
+    const job = await registrar.create(method, options);
+
     expect(job.jobId).toEqual(options.jobId);
   });
 });
@@ -56,12 +88,14 @@ describe('update identity', () => {
     didDocument
   };
 
-  nock(`${config.registrarUrl}/1.0/update`)
+  nock(config.registrarUrlUpdate)
     .post(`?method=${method}`, options)
     .reply(200, {jobId: '6d85bcd0-2ea3-4288-ab00-15afadd8a156'});
 
   it('should return identity creation job', async () => {
-    const job = await update(method, options);
+    const registrar = new Registrar();
+    const job = await registrar.update(method, options);
+
     expect(job.jobId).toEqual(options.jobId);
   });
 });
@@ -76,12 +110,14 @@ describe('deactivate identity', () => {
     didDocument
   };
 
-  nock(`${config.registrarUrl}/1.0/deactivate`)
+  nock(config.registrarUrlDeactivate)
     .post(`?method=${method}`, options)
     .reply(200, {jobId: '6d85bcd0-2ea3-4288-ab00-15afadd8a156'});
 
   it('should return identity creation job', async () => {
-    const job = await deactivate(method, options);
+    const registrar = new Registrar();
+    const job = await registrar.deactivate(method, options);
+
     expect(job.jobId).toEqual(options.jobId);
   });
 });
--- test/resolver.spec.js
+++ test/resolver.spec.js
@@ -1,17 +1,33 @@
+const {Resolver} = require('../src/resolver/Resolver');
+
 const nock = require('nock');
 
 const config = require('../src/config');
-const {resolve} = require('../src/resolver/resolver');
 
-describe('resolve identifier', () => {
+describe('setting a url', () => {
+  it('should use config / environment url when no url is provided', async () => {
+    const resolver = new Resolver();
+    expect(resolver.resolveUrl).toEqual(config.resolverUrlResolve);
+  });
+
+  it('should use given resolve url when provided by setter', async () => {
+    const otherResolver = 'https://other.resolver.io/1.0/identifiers';
+    const resolver = new Resolver();
+    resolver.setResolveUrl(otherResolver);
+    expect(resolver.resolveUrl).toEqual(otherResolver);
+  });
+});
+
+describe('identifier resolving', () => {
   const identifier = 'did:btcr:xz35-jznz-q6mr-7q6';
 
-  nock(config.resolverUrl)
-    .get(`/1.0/identifiers/${identifier}`)
+  nock(config.resolverUrlResolve)
+    .get(`/${identifier}`)
     .reply(200, {id: identifier});
 
-  it('should match did document id with identifier', async () => {
-    const didDDocument = await resolve(identifier);
-    expect(didDDocument.id).toEqual(identifier);
+  it('should resolve identifier to did document', async () => {
+    const resolver = new Resolver();
+    const didDocument = await resolver.resolve(identifier);
+    expect(didDocument.id).toEqual(identifier);
   });
 });

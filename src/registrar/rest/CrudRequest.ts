a1f73cca29e1ab7dfa916bda17901273538d4850
Bram ten Cate <btencate@sphereon.com>
2021-08-11 17:24:28 +0200

    ONTO-3 made CrudRequest immutable

--- src/registrar/rest/CrudRequest.ts
+++ src/registrar/rest/CrudRequest.ts
@@ -2,11 +2,13 @@
 
 import { DIDDocument } from 'did-resolver';
 
-export class CrudRequest {
-  public jobId: string;
-  public didDocument: DIDDocument;
-  public options: Record<string, unknown>;
-  public secret: Record<string, unknown> | string;
+import { ICrudRequest } from './ICrudRequest';
+
+export class CrudRequest implements ICrudRequest {
+  readonly jobId: string;
+  readonly didDocument: DIDDocument;
+  readonly options: Record<string, unknown>;
+  readonly secret: Record<string, unknown> | string;
 
   protected constructor(request: CrudRequest) {
     Object.assign(this, request);
